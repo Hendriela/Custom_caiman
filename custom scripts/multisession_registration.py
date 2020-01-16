fa07d8b0d9ea5ad197f50c5758fa66e8497e5e08
Hendriela <hendrik-heiser@gmx.de>
2020-01-16 18:08:43 +0100

    Plot updating with drawing one figure and updating it
    Signed-off-by: Hendriela <hendrik-heiser@gmx.de>

--- custom scripts/multisession_registration.py
+++ custom scripts/multisession_registration.py
@@ -7,11 +7,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as grid
 from skimage.feature import register_translation
-from scipy.ndimage import shift as shift_img
 from math import ceil
 from point2d import Point2D
+from copy import deepcopy
 
-#%%
+#%% LOADING AND AUTOMATICALLY ALIGNING MULTISESSION DATA
 
 
 def plot_all_aligned(spatial, templates, data_only=False):
@@ -79,89 +79,7 @@ def load_multisession_data(dir_list):
     dim = templates[0].shape  # dimensions of the FOV, can be gotten from templates
 
     return spatial, templates, dim, pcf_objects
-
-
-def manual_place_cell_alignment(pcf_objects):
-
-
-    # get indices of place cells from first session
-    place_cell_idx = [x[0] for x in pcf_objects[0].place_cells]
-
-    fig = plt.figure(figsize=(15, 8))  # draw figure
-    outer = grid.GridSpec(1, 2)        # initialize outer structure (two fields horizontally)
-
-    # draw reference cell in the big plot on the left
-    ref = grid.GridSpecFromSubplotSpec(1, 1, subplot_spec=outer[0])
-    ref_ax = fig.add_subplot(ref[0])
-    ref_com = draw_single_contour(ax=ref_ax,
-                                  spatial=pcf_objects[0].cnmf.estimates.A[:, place_cell_idx[0]],
-                                  template=pcf_objects[0].cnmf.estimates.Cn)
-    ref_ax.tick_params(labelbottom=False, labelleft=False)
-
-    ### Find cells in the next session(s) that have their center of mass near the reference cell (25 px distance) ###
-    max_dist = 25  # maximum radial distance of a cell to the reference cell in pixel
-
-    # Calculate offset of FOVs with phase correlation (skimage.feature.register_translation)
-    shift = register_translation(pcf_objects[0].cnmf.estimates.Cn, pcf_objects[1].cnmf.estimates.Cn,
-                                 upsample_factor=100, return_error=False)
-
-    # Correct reference center-of-mass by the shift to be better aligned with the FOV of the second session
-    ref_com_shift = shift_com(ref_com, shift, pcf_objects[0].cnmf.estimates.Cn.shape)
-    ref_point = Point2D(ref_com_shift[0], ref_com_shift[1])  # transform CoM into a Point2D to handle radial distances
-
-    # get CoM data for all cells in the second session
-    plt.figure()
-    all_contours = visualization.plot_contours(pcf_objects[1].cnmf.estimates.A, pcf_objects[1].cnmf.estimates.Cn)
-    plt.close()
-
-    # Go through all cells and select the ones that have a CoM near the reference cell, also remember their distance
-    near_contours = []
-    distances = []
-    for contour in all_contours:
-        new_point = Point2D(contour['CoM'][0], contour['CoM'][1])
-        rad_dist = (new_point - ref_point).r
-        # x_dist = abs(contour['CoM'][0] - ref_com_shift[0])
-        # y_dist = abs(contour['CoM'][1] - ref_com_shift[1])
-        if rad_dist <= max_dist:
-            near_contours.append(contour)
-            distances.append(rad_dist)
-
-    # sort the near contours by their distance to the reference CoM
-    near_contours_sort = [x for _, x in sorted(zip(distances, near_contours), key=lambda pair: pair[0])]
-
-    # draw possible matching cells in the plots on the right
-    if len(near_contours_sort)+1 <= 9:  # the +1 is for the button "no matches"
-        n_rows = ceil((len(near_contours_sort)+1)/3)    # number of rows necessary to plot near contours into 3 columns
-        candidates = outer[1].subgridspec(n_rows, 3)    # create grid layout
-        counter = 0                                     # counter that keeps track of plotted contour number
-        for row in range(n_rows):
-            for column in range(3):
-                curr_ax = fig.add_subplot(candidates[row, column], picker=True)  # picker enables clicking the subplot
-                try:
-                    curr_neuron = near_contours_sort[counter]['neuron_id']
-                    # plot the current candidate
-                    curr_cont = draw_single_contour(ax=curr_ax,
-                                                    spatial=pcf_objects[1].cnmf.estimates.A[:, curr_neuron],
-                                                    template=pcf_objects[1].cnmf.estimates.Cn)
-                    # the url property of the Axes is used as a tag to remember which neuron has been clicked
-                    plt.setp(curr_ax, url=curr_neuron)
-                    curr_ax.tick_params(labelbottom=False, labelleft=False)
-                    counter += 1
-                # if there are no more candidates to plot, make plot into a "no matches" button and mark it with '-1'
-                except IndexError:
-                    t = curr_ax.text(0.5, 0.5, 'No Matches', va='center', ha='center')
-                    plt.setp(curr_ax, url=-1)
-                    curr_ax.tick_params(labelbottom=False, labelleft=False)
-
-    def onpick(event):
-        this_plot = event.artist                    # save artist (axis) where the pick was triggered
-        neuron_id = plt.getp(this_plot, 'url')      # get the url label of the plot that shows which neuron was plotted
-        print(neuron_id)
-
-    plt.tight_layout()
-    fig.canvas.mpl_connect('pick_event', onpick)
-    fig.show()
-
+#%% SEMI-MANUALLY ALIGNING PLACE CELLS ACROSS SESSIONS
 
 def draw_single_contour(ax, spatial, template, half_size=50):
     """
@@ -227,6 +145,182 @@ def shift_com(com, shift, dims):
         if com_shift[coord] > dims[coord]:
             com_shift[coord] = dims[coord]
     return com_shift
+
+
+def manual_place_cell_alignment(pcf_sessions, ref_session=0):
+
+    def adj_idx(idx):
+        """
+        Adjusts index of pcf_sessions list to avoid referring to the reference session.
+        :param idx: Index of non-reference session
+        :return: adj_idx, actual index of requested session in pcf_sessions
+        """
+        adj_idx = idx
+        if adj_idx >= ref_session:
+            adj_idx += 1
+        return adj_idx
+
+    def draw_reference(ax, pcf, idx):
+        """
+        Draws reference cell into the provided Axes and returns its contour data.
+        :param ax: Axes where to draw the cell, here ref_ax
+        :param pcf: PCF object that contains the component data, here pcf_sessions[session]
+        :param idx: int, index of the neuron that should be drawn, here taken from place_cell_idx
+        :return: 1-length list containing dictionary of contour data
+        """
+        com = draw_single_contour(ax=ax, spatial=pcf.cnmf.estimates.A[:, place_cell_idx[idx]],
+                                  template=pcf.cnmf.estimates.Cn)
+        plt.setp(ax, url=idx, title=f'Session {ref_session + 1}')
+        ax.tick_params(labelbottom=False, labelleft=False)
+        return com
+
+    def find_target_cells(reference_com, session_contours, dims, fov_shift, max_dist=25):
+        """
+        Finds cells in tar_sess that are close to the reference cell
+        :param reference_com: tuple, (x,y) of center of mass of reference cell
+        :param session_contours: list of contour dicts holding contour information of all target cells
+        :param dims: tuple, (x,y) of FOV dimensions
+        :param fov_shift: (x,y) of FOV shift between reference and target session (from all_shifts)
+        :param max_dist: int, maximum radial distance between reference and target cell to be considered 'nearby'
+        :return: list of contours of nearby cells sorted by distance
+        """
+        # Correct reference center-of-mass by the shift to be better aligned with the FOV of the second session
+        reference_com_shift = shift_com(reference_com, fov_shift, dims)
+        ref_point = Point2D(reference_com_shift[0],
+                            reference_com_shift[1])  # transform CoM into a Point2D that can handle radial distances
+
+        # Go through all cells and select the ones that have a CoM near the reference cell, also remember their distance
+        near_contours = []
+        distances = []
+        for contour in session_contours:
+            new_point = Point2D(contour['CoM'][0], contour['CoM'][1])
+            rad_dist = (new_point - ref_point).r
+            if rad_dist <= max_dist:
+                near_contours.append(contour)
+                distances.append(rad_dist)
+
+        # return contours sorted by their distance to the reference CoM
+        return [x for _, x in sorted(zip(distances, near_contours), key=lambda pair: pair[0])]
+
+    def draw_target_cells(near_contours_sort, idx, update=False):
+        """
+        Draws target cells in the right part of the figure.
+        :param near_contours_sort: list, contains contour data of target cells (from find_target_cells)
+        :param idx: int, index of the target session the target cells belong to
+        :return:
+        """
+        real_idx = adj_idx(idx) # exchange the target-session specific idx into a pcf_sessions index
+        # draw possible matching cells in the plots on the right
+        if len(near_contours_sort)+1 <= 9:  # the +1 is for the button "no matches"
+            n_rows = ceil((len(near_contours_sort)+1)/3)    # number of rows needed to plot near contours into 3 columns
+            candidates = outer[1].subgridspec(n_rows, 3)    # create grid layout
+            counter = 0                                     # counter that keeps track of plotted contour number
+            for row in range(n_rows):
+                for column in range(3):
+                    curr_ax = fig.add_subplot(candidates[row, column], picker=True)  # picker enables clicking subplots
+                    if row == 0 and column == 1:
+                        curr_ax.set_title(f'Session {real_idx+1}')
+                    try:
+                        curr_neuron = near_contours_sort[counter]['neuron_id']
+                        # plot the current candidate
+                        curr_cont = draw_single_contour(ax=curr_ax,
+                                                        spatial=target_sessions[idx].cnmf.estimates.A[:, curr_neuron],
+                                                        template=target_sessions[idx].cnmf.estimates.Cn)
+                        # the url property of the Axes is used as a tag to remember which neuron has been clicked
+                        # as well as which target session it belonged to
+                        plt.setp(curr_ax, url=(real_idx, curr_neuron))
+                        curr_ax.tick_params(labelbottom=False, labelleft=False)
+                        counter += 1
+                    # if there are no more candidates to plot, make plot into a "no matches" button and mark it with -1
+                    except IndexError:
+                        t = curr_ax.text(0.5, 0.5, 'No Matches', va='center', ha='center')
+                        plt.setp(curr_ax, url=(real_idx, -1))
+                        curr_ax.tick_params(labelbottom=False, labelleft=False)
+        else:
+            #TODO Fix plotting of many cells, this is just a placeholder
+            candidates = outer[1].subgridspec(1, 1)    # create grid layout
+            curr_ax = fig.add_subplot(candidates, picker=True)  # picker enables clicking subplots
+            t = curr_ax.text(0.5, 0.5, 'More than 9 cells! Fix plotting!', va='center', ha='center')
+            plt.setp(curr_ax, url=-1)
+            curr_ax.tick_params(labelbottom=False, labelleft=False)
+
+    def draw_both_sides(ref_idx, targ_sess_idx):
+
+        # build figure
+        fig = plt.figure(figsize=(15, 8))  # draw figure
+        outer = grid.GridSpec(1, 2)  # initialize outer structure (two fields horizontally)
+        ref = grid.GridSpecFromSubplotSpec(1, 1, subplot_spec=outer[0])  # initialize reference plot
+        ref_ax = fig.add_subplot(ref[0])  # draw reference plot
+
+        # first draw the reference cell
+        ref_com = draw_reference(ref_ax, pcf_sessions[ref_session], ref_idx)
+
+        # Find cells in the next session(s) that have their center of mass near the reference cell
+        nearby_contours = find_target_cells(ref_com, all_contours[targ_sess_idx],
+                                            pcf_sessions[ref_idx].cnmf.estimates.Cn.shape,
+                                            all_shifts[targ_sess_idx])
+
+        # Draw target cells in the right plots
+        draw_target_cells(nearby_contours, targ_sess_idx)
+
+        return nearby_contours, ref_ax, fig
+
+    #######################################################################################
+    ##### THIS FIGURE AND DATA PREPARATION HAS TO BE DONE ONLY ONCE PER FUNCTION CALL #####
+
+    target_sessions = [x for j, x in enumerate(pcf_sessions) if j != ref_session]
+    # get indices of place cells from first session
+    place_cell_idx = [x[0] for x in pcf_sessions[ref_session].place_cells]
+
+    # initialize alignment array (#place cells X #sessions)
+    alignment = np.zeros((len(place_cell_idx), len(pcf_sessions)))  # +1 because the reference session has been popped
+
+    # get contour data of all cells and FOV shifts between reference and the other sessions with phase correlation
+    all_contours = []
+    all_shifts = []
+    for sess_idx in range(len(pcf_sessions)):
+        if sess_idx != ref_session:
+            sess = pcf_sessions[sess_idx]
+            all_shifts.append(register_translation(pcf_sessions[ref_session].cnmf.estimates.Cn, sess.cnmf.estimates.Cn,
+                                                   upsample_factor=100, return_error=False))
+            plt.figure()
+            all_contours.append(visualization.plot_contours(sess.cnmf.estimates.A, sess.cnmf.estimates.Cn))
+            plt.close()
+
+    # First drawing
+    near_contours, ref_ax, fig = draw_both_sides(0, 0)  # This means to draw the first reference (place) cell and the target cells
+
+    # Define what happens when a plot was clicked: update alignment accordingly and draw the next set of cells
+    def onpick(event):
+        global near_contours, ref_ax, fig
+        this_plot = event.artist                 # save artist (axis) where the pick was triggered
+        id_s = plt.getp(this_plot, 'url')        # get the IDs of the neuron that was clicked and of the current session
+        ref_id = plt.getp(ref_ax, 'url')         # get the url label (idx) of the reference cell
+        print(f'Reference cell {ref_id}, clicked neuron {id_s}.')
+
+        # update the alignment array with the clicked assignment
+        alignment[ref_id, 0] = place_cell_idx[ref_id]   # which ID did this place cell have?
+        if id_s[1] == -1:  # '-1' means "no match" has been clicked, fill spot with nan
+            alignment[ref_id, id_s[0]] = np.nan
+        else:
+            # assign the clicked neuron_id to the 'ref_id' reference cell in the 'sess_id' session
+            alignment[ref_id, id_s[0]] = id_s[1]
+
+
+        # if this is the last session, draw a new reference cell
+        if id_s[0]+1 == len(pcf_sessions):
+            near_contours, ref_ax, fig = draw_both_sides(ref_id+1, id_s[0]+1)
+        # else, only draw the target cells of the next session
+        else:
+            draw_target_cells(near_contours, id_s[0]+1, update=True)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
+
+    plt.tight_layout()
+    fig.canvas.mpl_connect('pick_event', onpick)
+    fig.show()
+
+
 
 #%%
 dir_list = [r'W:\Neurophysiology-Storage1\Wahl\Hendrik\PhD\Data\Batch2\M19\20191125\N2',
