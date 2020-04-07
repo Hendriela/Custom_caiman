5fd22abccb1fce19dc5928c01edc81d12eb9a008
Hendriela <hendrik-heiser@gmx.de>
2020-04-07 18:31:33 +0200

    Immigrated Click and DraggableColorbar classes for manual neuron selection gui.
    Signed-off-by: Hendriela <hendrik-heiser@gmx.de>

--- custom scripts/preprocess.py
+++ custom scripts/preprocess.py
@@ -1,4 +1,7 @@
-""" Function for preprocessing .tif movies (line shift and correction) """
+"""
+Function for preprocessing .tif movies (line shift and correction
+as well as draggable colorbar for manual_neuron_selection_gui)
+"""
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -110,3 +113,98 @@ def correct_line_shift_stack(stack, crop_left=5, crop_right=0, nr_samples=100, n
     stack = apply_shift_to_stack(stack, line_shift, crop_left=crop_left, crop_right=crop_right)
 
     return stack
+
+#%% Custom plotting classes for the manual selection GUI
+
+
+# class from https://stackoverflow.com/questions/48446351/distinguish-button-press-event-from-drag-and-zoom-clicks-in-matplotlib
+class Click():
+    def __init__(self, axes, func, second_axes = None, button=1):
+        self.ax = axes
+        self.ax1 = second_axes
+        self.func = func
+        self.button = button
+        self.press = False
+        self.move = False
+        self.c1 = self.ax.figure.canvas.mpl_connect('button_press_event', self.onpress)
+        self.c2 = self.ax.figure.canvas.mpl_connect('button_release_event', self.onrelease)
+        self.c3 = self.ax.figure.canvas.mpl_connect('motion_notify_event', self.onmove)
+
+    def onclick(self,event):
+        # print('Click')
+        if event.inaxes == self.ax or event.inaxes == self.ax1 :
+            if event.button == self.button:
+                # trigger the defined event function
+                self.func(event, self.ax, self.ax1)
+
+    def onpress(self,event):
+        self.press=True
+    def onmove(self,event):
+        if self.press:
+            self.move=True
+    def onrelease(self,event):
+        if self.press and not self.move:
+            self.onclick(event)
+        self.press=False; self.move=False
+
+
+### Class modified from http://www.ster.kuleuven.be/~pieterd/python/html/plotting/interactive_colorbar.html
+class DraggableColorbar(object):
+    def __init__(self, colorbar, axis, image, vmin=0, vmax=1):
+        self.cbar = colorbar
+        self.mappable = axis
+        self.press = None
+        self.cycle = sorted([i for i in dir(plt.cm) if hasattr(getattr(plt.cm,i),'N')])
+        self.index = self.cycle.index(self.cbar.get_cmap().name)
+        self.img = image
+        self.vmin = vmin
+        self.vmax = vmax
+
+    def connect(self):
+        """connect to all the events we need"""
+        self.cidpress = self.cbar.patch.figure.canvas.mpl_connect(
+            'button_press_event', self.on_press)
+        self.cidrelease = self.cbar.patch.figure.canvas.mpl_connect(
+            'button_release_event', self.on_release)
+        self.cidmotion = self.cbar.patch.figure.canvas.mpl_connect(
+            'motion_notify_event', self.on_motion)
+
+    def on_press(self, event):
+        """on button press we will see if the mouse is over us and store some data"""
+        if event.inaxes != self.cbar.ax: return
+        self.press = event.x, event.y
+
+    def on_motion(self, event):
+        'on motion we will move the rect if the mouse is over us'
+        if self.press is None: return
+        if event.inaxes != self.cbar.ax: return
+        xprev, yprev = self.press
+        dx = event.x - xprev
+        dy = event.y - yprev
+        self.press = event.x,event.y
+        #print 'x0=%f, xpress=%f, event.xdata=%f, dx=%f, x0+dx=%f'%(x0, xpress, event.xdata, dx, x0+dx)
+        scale = self.vmax - self.vmin
+        perc = 0.03
+        if event.button==1:
+            self.vmin -= (perc*scale)*np.sign(dy)
+            self.vmax -= (perc*scale)*np.sign(dy)
+        elif event.button==3:
+            self.vmin -= (perc*scale)*np.sign(dy)
+            self.vmax += (perc*scale)*np.sign(dy)
+        self.img.set_clim(self.vmin,self.vmax)
+        self.cbar.draw_all()
+        # self.mappable.set_norm(self.cbar.norm)
+        self.cbar.patch.figure.canvas.draw()
+
+
+    def on_release(self, event):
+        """on release we reset the press data"""
+        self.press = None
+        # self.mappable.set_norm(self.cbar.norm)
+        self.cbar.patch.figure.canvas.draw()
+
+    def disconnect(self):
+        """disconnect all the stored connection ids"""
+        self.cbar.patch.figure.canvas.mpl_disconnect(self.cidpress)
+        self.cbar.patch.figure.canvas.mpl_disconnect(self.cidrelease)
+        self.cbar.patch.figure.canvas.mpl_disconnect(self.cidmotion)
