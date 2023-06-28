88a13b5c5e2dcacf6d96a02f40c98f2d5dc7eb0d
Hendriela <hendrik-heiser@gmx.de>
2023-06-28 10:01:26 +0200

    Implement quantification of stability splits with transition matrices
    Signed-off-by: Hendriela <hendrik-heiser@gmx.de>

--- custom scripts/preprint/placecell_heatmaps.py
+++ custom scripts/preprint/placecell_heatmaps.py
@@ -215,7 +215,7 @@ queries = (
            # (common_match.MatchedIndex & 'mouse_id=93'),
            # (common_match.MatchedIndex & 'mouse_id=108' & 'day<"2022-09-09"'),
            # (common_match.MatchedIndex & 'mouse_id=110' & 'day<"2022-09-09"'),
-           (common_match.MatchedIndex & 'username="hheise"' & 'mouse_id=121' & 'day<"2022-09-09"'),
+           # (common_match.MatchedIndex & 'username="hheise"' & 'mouse_id=121' & 'day<"2022-09-09"'),
            # (common_match.MatchedIndex & 'mouse_id=115' & 'day<"2022-09-09"'),
            # (common_match.MatchedIndex & 'mouse_id=122' & 'day<"2022-09-09"'),
            # (common_match.MatchedIndex & 'mouse_id=114' & 'day<"2022-09-09"')
@@ -224,10 +224,10 @@ queries = (
 # Sham mice
 queries = (
            # (common_match.MatchedIndex & 'mouse_id=33' & 'day<="2020-08-24"'),
-           (common_match.MatchedIndex & 'mouse_id=121' & 'day<="2022-08-12"'),
-           (common_match.MatchedIndex & 'mouse_id=115' & 'day<="2022-08-09"'),
-           (common_match.MatchedIndex & 'mouse_id=122' & 'day<="2022-08-09"'),
-           # (common_match.MatchedIndex & 'mouse_id=114' & 'day<="2022-08-09"')
+           (common_match.MatchedIndex & 'mouse_id=115' & 'day<"2022-09-09"'),
+           (common_match.MatchedIndex & 'mouse_id=122' & 'day<"2022-09-09"'),
+           # (common_match.MatchedIndex & 'mouse_id=121' & 'day<"2022-09-09"'),
+    # (common_match.MatchedIndex & 'mouse_id=114' & 'day<="2022-08-09"')
 )
 
 is_pc = []
@@ -257,13 +257,13 @@ for query in queries:
                                                 return_array=True, relative_dates=True,
                                                 surgery='Microsphere injection'))
 
-# Stack together data from different mice
+# Stack together data from different mice (deficit)
 spat_dff = []
 spat_dff.append(get_place_cells(is_pc_arr=is_pc[0]['41_1'], spat_dff_arr=spat_dff_maps[0]['41_1'], pc_day=-1, select_days=[-5, -4, -1, 2, 8, 17]))
 spat_dff.append(get_place_cells(is_pc[1]['69_1'], spat_dff_maps[1]['69_1'], pc_day=0, select_days=[-2, -1, 0, 3, 9, 15]))
 spat_dff.append(get_place_cells(is_pc[2]['85_1'], spat_dff_maps[2]['85_1'], pc_day=0, select_days=[-2, -1, 0, 3, 8, 17]))
 spat_dff.append(get_place_cells(is_pc[3]['90_1'], spat_dff_maps[3]['90_1'], pc_day=0, select_days=[-2, -1, 0, 3, 8, 17]))
-spat_dff.append(get_place_cells(is_pc_arr=is_pc[4]['121_1'], spat_dff_arr=spat_dff_maps[4]['121_1'], pc_day=0, select_days=[-2, -1, 0, 3, 9, 18]))
+# spat_dff.append(get_place_cells(is_pc_arr=is_pc[4]['121_1'], spat_dff_arr=spat_dff_maps[4]['121_1'], pc_day=0, select_days=[-2, -1, 0, 3, 9, 18]))
 spat_dff = np.vstack(spat_dff)
 
 spat_dff_noncoding = []
@@ -271,9 +271,25 @@ spat_dff_noncoding.append(get_noncoding(is_pc[0]['41_1'], spat_dff_maps[0]['41_1
 spat_dff_noncoding.append(get_noncoding(is_pc[1]['69_1'], spat_dff_maps[1]['69_1'], select_days=[-2, -1, 0, 3, 9, 15]))
 spat_dff_noncoding.append(get_noncoding(is_pc[2]['85_1'], spat_dff_maps[2]['85_1'], select_days=[-2, -1, 0, 3, 8, 17]))
 spat_dff_noncoding.append(get_noncoding(is_pc[3]['90_1'], spat_dff_maps[3]['90_1'], select_days=[-2, -1, 0, 3, 8, 17]))
-spat_dff_noncoding.append(get_noncoding(is_pc[4]['121_1'], spat_dff_maps[4]['121_1'], select_days=[-2, -1, 0, 3, 9, 18]))
+# spat_dff_noncoding.append(get_noncoding(is_pc[4]['121_1'], spat_dff_maps[4]['121_1'], select_days=[-2, -1, 0, 3, 9, 18]))
 spat_dff_noncoding, noncoding_masks = list(map(list, zip(*spat_dff_noncoding)))
 spat_dff_noncoding = np.vstack(spat_dff_noncoding)
+
+
+# Stack together data from different mice (sham)
+spat_dff = []
+spat_dff.append(get_place_cells(is_pc_arr=is_pc[0]['115_1'], spat_dff_arr=spat_dff_maps[0]['115_1'], pc_day=0, select_days=[-2, -1, 0, 3, 9, 18]))
+spat_dff.append(get_place_cells(is_pc_arr=is_pc[1]['122_1'], spat_dff_arr=spat_dff_maps[1]['122_1'], pc_day=0, select_days=[-2, -1, 0, 3, 9, 18]))
+# spat_dff.append(get_place_cells(is_pc_arr=is_pc[2]['121_1'], spat_dff_arr=spat_dff_maps[2]['121_1'], pc_day=0, select_days=[-2, -1, 0, 3, 9, 18]))
+spat_dff = np.vstack(spat_dff)
+
+spat_dff_noncoding = []
+spat_dff_noncoding.append(get_noncoding(is_pc[0]['115_1'], spat_dff_maps[0]['115_1'], select_days=[-2, -1, 0, 3, 9, 18]))
+spat_dff_noncoding.append(get_noncoding(is_pc[1]['122_1'], spat_dff_maps[1]['122_1'], select_days=[-2, -1, 0, 3, 9, 18]))
+# spat_dff_noncoding.append(get_noncoding(is_pc[2]['121_1'], spat_dff_maps[2]['121_1'], select_days=[-2, -1, 0, 3, 9, 18]))
+spat_dff_noncoding, noncoding_masks = list(map(list, zip(*spat_dff_noncoding)))
+spat_dff_noncoding = np.vstack(spat_dff_noncoding)
+
 
 # split dataset into stable (upper 50%) and unstable (lower 50%) cells
 upper_percentile = 50
@@ -304,6 +320,148 @@ Todo: Does this make sense? We would have to decide how often a place cell shoul
 And the quantification will most likely be done on individual days as well, so the plotting should reflect that.
 """
 
-########################################################################################################################
+############################################### QUANTIFICATION #########################################################
 
 
+def quantify_stability_split(is_pc_arr, spat_dff_arr, rel_days, day_diff = 3, stab_thresh = None):
+    """
+    Split cells into non-coding (never a PC) and coding (at least once a PC). Compute stability across prestroke
+    sessions for all coding cells. Baseline threshold for the network is the median correlation of all PCs.
+    Get ratio of non-coding - unstable - stable place cells.
+    Called once per mouse.
+
+    Args:
+        is_pc_arr: is_pc output for one mouse. Already restricted for a certain time period.
+        spat_dff_arr: spat_dff output for one mouse.  Already restricted for a certain time period.
+    """
+
+    # Filter cells that are place cells at least once
+    filter_pc = np.nansum(is_pc_arr, axis=1) > 0
+
+    filter_idx = np.where(filter_pc)[0]   # Indices of place cells to keep track of their attributes
+
+    # Compute stability across all session pairs of all place cells
+    stab = []
+    for cell_data in spat_dff_arr[filter_pc]:
+        cell_corr = []
+        for day_idx, day in enumerate(rel_days):
+            next_day_idx = np.where(rel_days == day + day_diff)[0]
+            if len(next_day_idx) == 1:
+                cell_corr.append(np.corrcoef(cell_data[day_idx], cell_data[next_day_idx[0]])[0, 1])
+        stab.append(np.nanmean(cell_corr))
+
+    # Some cells did not occur in days with distance day_diff, they have to be excluded as well
+    filter_idx = filter_idx[~np.isnan(stab)]
+    stab = np.array(stab)[~np.isnan(stab)]
+
+    # If not provided (for prestroke period), get median of stability (threshold for further stability classifications)
+    if stab_thresh is None:
+        stab_thresh = np.median(stab)
+        return_stab = True
+    else:
+        return_stab = False
+
+    # Get numbers and ratio of non-coding, unstable and stable cells, as well as classification mask
+    stable_mask = np.zeros(len(is_pc_arr), dtype=bool)
+    stable_mask[filter_idx[stab > stab_thresh]] = 1
+    unstable_mask = np.zeros(len(is_pc_arr), dtype=bool)
+    unstable_mask[filter_idx[stab <= stab_thresh]] = 1
+
+    noncoding_mask = np.nansum(is_pc_arr, axis=1) == 0
+    noncoding_mask[np.isnan(is_pc_arr).sum(axis=1) == is_pc_arr.shape[1]] = False   # Exclude cells that were not present at all
+
+    """    
+    Construct single mask with cell classification:
+        Class 0: Rare cells that did not appear in enough sessions to be analysed (cells that did not appear in period 
+            at all, and place cells that did not appear on days with distance day_diff).
+        Class 1: Non-coding cells (cells that are never a PC in the period)
+        Class 2: Cells that are a PC in at least one session during the period and have an average cross-session 
+            stability below the threshold.
+        Class 3: Cells that are a PC in at least one session during the period and have an average cross-session 
+            stability above the threshold.
+    """
+    class_mask = np.zeros(len(is_pc_arr), dtype=int)
+    class_mask[noncoding_mask] = 1
+    class_mask[unstable_mask] = 2
+    class_mask[stable_mask] = 3
+
+    if return_stab:
+        return class_mask, stab_thresh
+    else:
+        return class_mask
+
+
+def summarize_quantification(class_mask, period='None'):
+
+    return pd.DataFrame([dict(n0=np.sum(class_mask == 0), n1=np.sum(class_mask == 1),
+                              n2=np.sum(class_mask == 2), n3=np.sum(class_mask == 3),
+                              n0_r=(np.sum(class_mask == 0)/len(class_mask))*100,
+                              n1_r=(np.sum(class_mask == 1)/len(class_mask))*100,
+                              n2_r=(np.sum(class_mask == 2)/len(class_mask))*100,
+                              n3_r=(np.sum(class_mask == 3)/len(class_mask))*100,
+                              period=period)])
+
+
+def transition_matrix(mask1, mask2, num_classes=4, percent=True):
+
+    # Create empty transition matrix
+    trans = np.zeros((num_classes, num_classes), dtype=int)
+
+    for prev_class, curr_class in zip(mask1, mask2):
+        trans[prev_class, curr_class] += 1
+
+    if percent:
+        trans = (trans/len(mask1)) * 100
+
+    return trans
+
+
+mice = ['41_1', '69_1', '85_1', '90_1']
+dfs = []
+for i, mouse in enumerate(mice):
+    rel_dates = np.array(is_pc[i][mouse][1])
+    mask_pre = rel_dates <= 0
+    mask_early = (0 < rel_dates) & (rel_dates <= 9)
+    mask_late = rel_dates > 9
+
+    classes_pre, stability_thresh = quantify_stability_split(is_pc_arr=is_pc[i][mouse][0][:, mask_pre],
+                                                             spat_dff_arr=spat_dff_maps[i][mouse][0][:, mask_pre],
+                                                             rel_days=rel_dates[mask_pre])
+    classes_early = quantify_stability_split(is_pc_arr=is_pc[i][mouse][0][:, mask_early],
+                                             spat_dff_arr=spat_dff_maps[i][mouse][0][:, mask_early],
+                                             rel_days=rel_dates[mask_early], stab_thresh=stability_thresh)
+    classes_late = quantify_stability_split(is_pc_arr=is_pc[i][mouse][0][:, mask_late],
+                                            spat_dff_arr=spat_dff_maps[i][mouse][0][:, mask_late],
+                                            rel_days=rel_dates[mask_late], stab_thresh=stability_thresh)
+
+    df = pd.concat([summarize_quantification(classes_pre, 'pre'),
+                    summarize_quantification(classes_early, 'early'),
+                    summarize_quantification(classes_late, 'late')])
+    df['mouse_id'] = mouse
+    df['classes'] = [classes_pre, classes_early, classes_late]
+    dfs.append(df)
+class_df = pd.concat(dfs)
+
+# Plot transition matrices
+
+
+def label_row(label, axis, pad=5):
+    axis.annotate(label, xy=(0, 0.5), xytext=(-axis.yaxis.labelpad - pad, 0), xycoords=axis.yaxis.label,
+                  textcoords='offset points', size='large', ha='right', va='center')
+
+
+fig, ax = plt.subplots(2, len(class_df.mouse_id.unique()), layout='constrained')
+for i, mouse in enumerate(class_df.mouse_id.unique()):
+    mask_pre = class_df[(class_df['mouse_id'] == mouse) & (class_df['period'] == 'pre')]['classes'].iloc[0]
+    mask_early = class_df[(class_df['mouse_id'] == mouse) & (class_df['period'] == 'early')]['classes'].iloc[0]
+    mask_late = class_df[(class_df['mouse_id'] == mouse) & (class_df['period'] == 'late')]['classes'].iloc[0]
+    sns.heatmap(transition_matrix(mask_pre, mask_early), ax=ax[0, i], square=True, annot=True, cbar=False)
+    sns.heatmap(transition_matrix(mask_early, mask_late), ax=ax[1, i], square=True, annot=True, cbar=False)
+    ax[0, i].set_title(mouse)
+
+# label_row('Pre->Early', ax[0, 0])
+# label_row('Early->Late', ax[1, 0])
+ax[0, 0].set_xlabel('To Cell Class Early')
+ax[0, 0].set_ylabel('From Cell Class Pre')
+ax[1, 0].set_xlabel('To Cell Class Late')
+ax[1, 0].set_ylabel('From Cell Class Early')
