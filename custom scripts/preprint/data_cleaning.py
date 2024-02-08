6f0f91ec77212e725cc52201e3c56586933133df
Hendriela <hendrik-heiser@gmx.de>
2024-02-08 15:35:10 +0100

    Extract merge_df function from neural_sphere_correlation.py and neuron_synchronicity.py
    Signed-off-by: Hendriela <hendrik-heiser@gmx.de>

--- custom scripts/preprint/data_cleaning.py
+++ custom scripts/preprint/data_cleaning.py
@@ -245,6 +245,53 @@ def load_data(data_type,
     return data
 
 
+def merge_dfs(df, sphere_df, inj_df, vr_df=None):
+
+    df_merge = pd.merge(df, sphere_df, on='mouse_id')
+    df_merge = pd.merge(df_merge, inj_df, on='mouse_id')
+
+    df_merge['rel_day'] = df_merge.apply(lambda x: (x['day'] - x['surgery_date']).days, axis=1)
+
+    rel_days_align = []
+    for mouse_id, mouse_df in df_merge.groupby('mouse_id'):
+        rel_days = mouse_df.rel_day.copy()
+        if 3 not in rel_days:
+            rel_days[(rel_days == 2) | (rel_days == 4)] = 3
+        rel_days[(rel_days == 5) | (rel_days == 6) | (rel_days == 7)] = 6
+        rel_days[(rel_days == 8) | (rel_days == 9) | (rel_days == 10)] = 9
+        rel_days[(rel_days == 11) | (rel_days == 12) | (rel_days == 13)] = 12
+        rel_days[(rel_days == 14) | (rel_days == 15) | (rel_days == 16)] = 15
+        rel_days[(rel_days == 17) | (rel_days == 18) | (rel_days == 19)] = 18
+        rel_days[(rel_days == 20) | (rel_days == 21) | (rel_days == 22)] = 21
+        rel_days[(rel_days == 23) | (rel_days == 24) | (rel_days == 25)] = 24
+        if 28 not in rel_days:
+            rel_days[(rel_days == 26) | (rel_days == 27) | (rel_days == 28)] = 27
+        rel_sess = np.arange(len(rel_days)) - np.argmax(np.where(rel_days <= 0, rel_days, -np.inf))
+        rel_days[(-5 < rel_sess) & (rel_sess < 1)] = np.arange(-np.sum((-5 < rel_sess) & (rel_sess < 1))+1, 1)
+        rel_days.name = 'rel_day_align'
+        rel_days_align.append(rel_days)
+    rel_days_align = pd.concat(rel_days_align)
+    df_merge = df_merge.join(rel_days_align)
+
+    # Do not analyze day 1
+    df_merge = df_merge[df_merge.rel_day_align != 1]
+
+    def get_phase(row):
+        if row.rel_day_align <= 0:
+            return 'pre'
+        elif row.rel_day_align <= 7:
+            return 'early'
+        else:
+            return 'late'
+    df_merge['phase'] = df_merge.apply(get_phase, axis=1)
+
+    if vr_df is not None:
+        df_merge = pd.merge(df_merge, vr_df, on=['mouse_id', 'day'])
+
+    return df_merge
+
+
+
 def fix_place_cell_ratio():
 
     ### No mismatch for SpatialInformation place_cell_ratio
